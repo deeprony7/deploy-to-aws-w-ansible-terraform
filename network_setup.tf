04891218275d778d347d7b41f3e364b696978dd9
Shouvick Halder <deeprony7@gmail.com>
2021-02-22 00:05:48 +0530

    added igws and accepted peering req

--- network_setup.tf
+++ network_setup.tf
@@ -21,3 +21,34 @@ resource "aws_vpc" "vpc_master_oregon" {
   }
 
 }
+
+# Initate VPC peering connection request from us-east-1
+resource "aws_vpc_peering_connection" "useast1-uswest2" {
+  provider    = aws.region-master
+  peer_vpc_id = aws_vpc.vpc_master_oregon.id
+  vpc_id      = aws_vpc.vpc_master.id
+  peer_region = var.region-worker
+  #   auto_accept = true
+  tags = {
+    Name = "useast1-uswest2"
+  }
+}
+
+# Create IGW in us-east-1
+resource "aws_internet_gateway" "igw-master" {
+  provider = aws.region-master
+  vpc_id   = aws_vpc.vpc_master.id
+}
+
+# Create IGW in us-east-1
+resource "aws_internet_gateway" "igw-oregon" {
+  provider = aws.region-worker
+  vpc_id   = aws_vpc.vpc_master_oregon.id
+}
+
+# Accept VPC peering request in us-west-2 from us-east-1
+resource "aws_vpc_peering_connection_accepter" "accept_peering" {
+  provider                  = aws.region-worker
+  vpc_peering_connection_id = aws_vpc_peering_connection.useast1-uswest2.id
+  auto_accept               = true
+}
